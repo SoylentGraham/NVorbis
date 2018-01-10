809a38512a65aa28e2e417a4b48898aaf034624d
Graham Reeves <graham@newchromantics.com>
2018-01-10 20:56:57 +0000

    Added InvalidDataException (Unity can't find it in System.IO)

---
+++ NVorbis/InvalidDataException.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+/*
+ * gr: for some reason, unity cannot resolve this exception from System.IO...
+*/
+public class InvalidDataException : Exception
+{
+	public InvalidDataException(string Message="") : base(Message)
+	{
+	}
+}
